8a08088f3659c9215d0f39108984d9966dcb7810
Ruslan Dinullin <ruslan@dinullin.xyz>
2020-10-25 13:15:08 +0500

    Небольшое исрпавление в getFine

--- pages/api/fines.js
+++ pages/api/fines.js
@@ -3,12 +3,7 @@ const ENDPOINT = 'https://test-task.shtrafovnet.com';
 export default async function getFine(uin, setLoading) {
     setLoading(true);
     const req = await fetch(ENDPOINT + '/fines/' + uin.toString());
-    let resp;
-    if (req.ok) {
-        resp = req.json()
-    } else {
-        resp = 'error'
-    }
+    const resp = req.ok ? req.json() : 'error';
     setLoading(false);
     return resp
 }
